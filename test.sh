c611ed4e61dce22b9422e3644ca1781266528f53
ImadlakQvQ <472974582@qq.com>
2024-11-24 23:13:29 -0500

    modified

--- test.sh
+++ test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --account=def-bboulet     # set account
 #SBATCH --gpus-per-node=4         # Number of GPU(s) per node
-#SBATCH --output=runlog/exp.out      # log 保存地址
+#SBATCH --output=runlog/log_%j.out      # log 保存地址
 #SBATCH --cpus-per-task=6         # CPU cores/threads
 #SBATCH --mem=80gb               # memory per node
 #SBATCH --time=1-10:10            # set the time for tasks     3 days 2 hours 1 minute 0 second for --time==3-02:01:00
 
-module load StdEnv/2023  gcc/12.3 cuda/12.2 arrow/17.0 rust/1.70.0 python/3.10.13              # load the module
+module load StdEnv/2023  gcc/12.3 cuda/12.2 arrow/17.0 rust/1.70.0 python/3.10.13 git-lfs/3.4.0             # load the module
 #cd /project/def-bboulet/imadlak/program/VinePPO                                                # set the path
 source venv/bin/activate                                                                       # activate the env
 
